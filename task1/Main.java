7bec52d720648f1db02938aa18745b24b461f9e4
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2023-09-14 18:58:16 +0300

    Update Main.java

--- task1/Main.java
+++ task1/Main.java
@@ -1,1 +1,20 @@
 
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Программа подсчета реальной цены");
+        Calculator Cost = new Calculator();
+
+        int Sum = 100;
+        double discount = 0.1;
+
+        double RealCost = Cost.calculateDiscount(Sum, discount);
+        System.out.println("Реальная цена с учетом скидки: " + RealCost);
+    }
+}
+
+
+
+
+
+
